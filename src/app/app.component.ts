dab1f18e314676cb99c88a739e68fdb03a191d85
Daniel Hedrick <misterhedrick@gmail.com>
2019-02-11 13:45:25 -0500

    starting node express part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  storedPosts = [];
 
-  onPostAdded(post) {
-    this.storedPosts.push(post);
-  }
 
 }
