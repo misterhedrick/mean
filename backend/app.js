253696cf86d6e25ab3bd4d6aef228c84844b0b31
Daniel Hedrick <misterhedrick@gmail.com>
2019-02-14 09:36:43 -0500

    adding and deleting with mongodb

--- backend/app.js
+++ backend/app.js
@@ -30,10 +30,13 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message: 'Post added successfully'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost.id
+    });
   });
+
 });
 
 
@@ -46,7 +49,13 @@ app.get('/api/posts', (req, res, next) => {
         posts: documents
       });
     });
+});
 
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "post deleted!" });
+  });
 });
 
 module.exports = app;
