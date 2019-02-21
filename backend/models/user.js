800fa71dccd91cda625ff062525e4a7d72000495
Daniel Hedrick <misterhedrick@gmail.com>
2019-02-21 08:52:07 -0500

    fixed user schema issue

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,4 +5,4 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true }
 });
 userSchema.plugin(uniqueValidator);
-module.exports = mongoose.model('Post', userSchema);
+module.exports = mongoose.model('User', userSchema);
